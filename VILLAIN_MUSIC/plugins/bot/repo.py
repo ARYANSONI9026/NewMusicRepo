befab103fd1760442adb0d2865bc90cbe453992d
ARYANSONI9026 <soniaryan2240021@gmail.com>
2025-07-14 06:44:27 +0530

    Update repo.py

--- VILLAIN_MUSIC/plugins/bot/repo.py
+++ VILLAIN_MUSIC/plugins/bot/repo.py
@@ -29,11 +29,11 @@ async def start(_, msg):
           InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
         ],
         [
-          InlineKeyboardButton("ʜᴇʟᴘ", url="https://t.me/knight_society2"),
-          InlineKeyboardButton("ᴀɴɪᴍᴀʟ", url="https://t.me/tabahi_tabahi"),
+          InlineKeyboardButton("ʜᴇʟᴘ", url="https://t.me/tabahi_tabahi"),
+          InlineKeyboardButton("ᴀɴɪᴍᴀʟ", url="https://t.me/smartness_to_hai"),
           ],
                [
-                InlineKeyboardButton("ᴀɴɪᴍᴀʟ ꭙ ꜱᴜᴘᴘᴏʀᴛ˼", url=f"https://t.me/iamvillain77"),
+                InlineKeyboardButton("ᴀɴɪᴍᴀʟ ꭙ ꜱᴜᴘᴘᴏʀᴛ˼", url=f"https://t.me/+e6OL4yuY88Y3ZTI1"),
 ],
 [
 InlineKeyboardButton("ᴍᴀɪɴ ʙᴏᴛ", url=f"https://t.me/Shizuka_Music_X_Bot"),
