fd1fd60ccc1b3dfb516138bf79e47a9d1acbd8f0
ARYANSONI9026 <soniaryan2240021@gmail.com>
2025-07-14 06:06:15 +0530

    Update ping.py

--- VILLAIN_MUSIC/plugins/tools/ping.py
+++ VILLAIN_MUSIC/plugins/tools/ping.py
@@ -1,28 +1,36 @@
 from datetime import datetime
-
 from pyrogram import filters
-from pyrogram.types import Message
-
+from pyrogram.types import Message, InlineKeyboardMarkup
 from VILLAIN_MUSIC import app
-from VILLAIN_MUSIC.core.call import Hotty
+from VILLAIN_MUSIC.core.call import VILLAIN
 from VILLAIN_MUSIC.utils import bot_sys_stats
+from VILLAIN_MUSIC.utils.inline.extras import botplaylist_markup
 from VILLAIN_MUSIC.utils.decorators.language import language
-from VILLAIN_MUSIC.utils.inline import supp_markup
 from config import BANNED_USERS, PING_IMG_URL
+import aiohttp
+import asyncio
 
-
-@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+ 
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS & filters.group)
 @language
 async def ping_com(client, message: Message, _):
-    start = datetime.now()
     response = await message.reply_photo(
         photo=PING_IMG_URL,
         caption=_["ping_1"].format(app.mention),
     )
-    pytgping = await Hotty.ping()
+    start = datetime.now()
+    pytgping = await VILLAIN.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
-    await response.edit_text(
-        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
-        reply_markup=supp_markup(_),
+    await response.edit(
+        _["ping_2"].format(
+            resp,
+            app.mention,
+            UP,
+            RAM,
+            CPU,
+            DISK,
+            pytgping,
+        ),
+        reply_markup=InlineKeyboardMarkup(botplaylist_markup(_)),
     )
